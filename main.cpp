63289828c70287e3d908c4722a42db1cf3c3ea30
au2steja-cpu <au2steja@gmail.com>
2026-09-16 20:09:55 +0300

    initial commit - added struct and main file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Studentas {
+	string vardas;
+	string pavarde;
+	vector<int> nd;
+	int egz;
+};
+
+int main() {
+	cout << "Programa paleista sekmingai" << endl;
+
+	return 0;
+}
