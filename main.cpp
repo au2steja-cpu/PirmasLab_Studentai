1f093bb9dcada7368a4b61c0e6c56b87ca97f0bf
au2steja-cpu <au2steja@gmail.com>
2026-09-17 17:59:56 +0300

    Commit 1: Pradine struktura ir meniu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iomanip>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
 
 using namespace std;
 
@@ -9,10 +16,29 @@ struct Studentas {
 	string pavarde;
 	vector<int> nd;
 	int egz;
+	double galutinisVid = 0.0;
+	double galutineMed = 0.0;
 };
 
 int main() {
-	cout << "Programa paleista sekmingai" << endl;
+	vector<Studentas> studentai;
+	int pasirinkimas;
+
+	while (true) {
+		cout << " Meniu";
+		cout << " Ivesti studentus rankiniu budu";
+		cout << " Generuoti studentus atsitiktinai";
+		cout << " Spausdinti rezultatus";
+		cout << " Issaugoti rezultatus i faila";
+		cout << " Baigti darba";
+		cout << " Pasirinkti veiksma: ";
+		cin >> pasirinkimas;
+
+		if (pasirinkimas == 0) {
+			cout << "Programa baigia darba";
+			return 0;
+		}
+	}
 
 	return 0;
-}
+} 
