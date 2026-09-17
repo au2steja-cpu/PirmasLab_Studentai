90e894ac4bab6d1d1c6d37d798c3731b26684900
au2steja-cpu <au2steja@gmail.com>
2026-09-17 21:21:26 +0300

    mediana

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ struct Studentas {
 	vector<int> nd_rezultatai;
 	int egz;
 	double galutinis_vid;
+	double galutine_med;
 };
 
 double skaiciutiVidurki(const vector<int>& nd, int egz) {
@@ -25,4 +26,17 @@ double skaiciutiVidurki(const vector<int>& nd, int egz) {
 	}
 	double vidurkis = sum / nd.size();
 	return (vidurkis * 0.4) + (egz * 0.6);
+}
+
+double skaiciuotiMediana(vector<int> nd, int egz) {
+	if (nd.empty()) return egz * 0.6;
+	sort(nd.begin(), nd.end());
+	size_t size = nd.size();
+	double mediana;
+	if (size % 2 == 0) {
+		mediana = (nd[size / 2 - 1] + nd[size / 2]) / 2.0;
+	} else {
+		mediana = nd[size / 2];
+	}
+	return (mediana * 0.4) + (egz * 0.6);
 }
